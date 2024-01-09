b5b6884046af349b4d6c616dd41a3f9ae82ab03e
Alex Lemay <alemay8@hotmail.ca>
2024-01-09 00:07:40 -0500

    Add the player and the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //console.log("Hello World !")//
+
+function getComputerChoice(){
+    let number = Math.floor(Math.random()* 3)
+    if (number == 0){
+        return "rock"
+    }
+    else if (number == 1){
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+//console.log(getComputerChoice())//
+//Still good :)//
+let computerSelection = getComputerChoice()
+
+function getPlayerChoice(){
+    let choice = prompt("Rock, paper or scissors ?");
+    let selection = choice.toLowerCase();
+    if (selection === "rock"){
+        //return selection;//
+        console.log(selection);
+    }
+    else if(selection ==="paper"){
+        //return selection;//
+        console.log(selection);
+    }
+    else if(selection ==="scissors"){
+        //return selection;//
+        console.log(selection);
+    }
+    else {
+        console.log("this value is invalid !");
+    }
+
+}
+getPlayerChoice()
